169acd321ad8de28aef4f6049f95f65681e8e649
Samyeak <10978827+Samyeak@users.noreply.github.com>
2020-09-30 09:24:47 +0545

    Converted to Windows Service

--- WorkerServicePoC/Program.cs
+++ WorkerServicePoC/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NLog;
+using NLog.Extensions.Logging;
 
 namespace WorkerServicePoC
 {
@@ -16,6 +18,15 @@ namespace WorkerServicePoC
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddNLog(new NLogProviderOptions
+                    {
+                        CaptureMessageProperties = true,
+                        CaptureMessageTemplates = true
+                    });
+                })
                 .ConfigureServices((hostContext, services) =>
                 {
                     services.AddHostedService<Worker>();
--- WorkerServicePoC/Worker.cs
+++ WorkerServicePoC/Worker.cs
@@ -1,8 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -11,10 +14,34 @@ namespace WorkerServicePoC
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration Configuration;
 
-        public Worker(ILogger<Worker> logger)
+        private string ConnectionString;
+
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            Configuration = configuration;
+        }
+
+        public override async Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                ConnectionString = Configuration["Client:Connection"];
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            await base.StartAsync(cancellationToken);
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            // DO YOUR STUFF HERE
+            await base.StopAsync(cancellationToken);
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
@@ -22,7 +49,35 @@ namespace WorkerServicePoC
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                CreateFile();
+                await Task.Delay(TimeSpan.FromSeconds(25), stoppingToken);
+            }
+        }
+
+        private void CreateFile()
+        {
+            string path = @"C:\Test";
+
+            string randomFileName = System.IO.Path.GetRandomFileName();
+            string filePath = System.IO.Path.Combine(path, randomFileName);
+            System.IO.File.Create(filePath);
+        }
+
+        private async Task ExecuteTransactionQueue()
+        {
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                using (SqlCommand cmd = connection.CreateCommand())
+                {
+                    if (connection.State != ConnectionState.Open)
+                    {
+                        connection.Open();
+                    }
+                    cmd.CommandText = "USP_TRIGGER_TRANSACTION_SCHEDULE_PROCESS";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandTimeout = 150;
+                    await cmd.ExecuteNonQueryAsync();
+                }
             }
         }
     }
